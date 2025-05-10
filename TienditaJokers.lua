724c1cc1fbfb4168e1b3132ded8d1e6fde92bb8c
KROOOL <artsalsoto1702@gmail.com>
2025-05-09 23:32:19 -0600

    0.2.2

--- TienditaJokers.lua
+++ TienditaJokers.lua
@@ -6,18 +6,18 @@ SMODS.Atlas {
 }
 
 SMODS.Joker {
-    key = 'yu_sze',
+    key = 'j_yu_sze',
     loc_txt = {
         name = "Yu Sze", --Nombre
         text = { --Informacion sobre lo q hace
-            "Gains {X:mult,C:white} X#2# {} Mult if played",
-            "hand has {C:attention}Stone{} card",
+            "Gains {X:mult,C:white} X#2# {} Mult each time",
+            "a {C:attention}Stone{} card is scored",
             "{C:inactive}(Currently {X:mult,C:white} X#1# {C:inactive} Mult)",
         },
     },
     atlas = 'TienditaJokers',
     rarity = 4, --rareza 1=comun 2=inusual 3=raro 4=legendario
-    cost = 1, --cuanto vale en la tienda
+    cost = 20, --cuanto vale en la tienda
     blueprint_compat = true, 
     eternal_compat = true,
     unlocked = true, --Desbloqueado por default
@@ -35,19 +35,27 @@ SMODS.Joker {
         }
     end,
     calculate = function(self, card, context)
-
-        if context.before and context.cardarea == G.jokers and not context.blueprint then
-            for _, v in ipairs(context.full_hand) do
-                if SMODS.has_enhancement(v, "m_stone") then
-                    card.ability.extra.x_mult = card.ability.extra.x_mult + card.ability.extra.x_mult_gain
-                end
+        if context.individual and context.cardarea == G.play and SMODS.has_enhancement(context.other_card, "m_stone") and not context.blueprint then
+                card.ability.extra.x_mult = card.ability.extra.x_mult + card.ability.extra.x_mult_gain 
+                return {
+                    message = localize('k_upgrade_ex'),
+                    colour = G.C.MULT,
+                    message_card = card
+             }
+        end
+        if context.joker_main then
+            return {
+                xmult = card.ability.extra.x_mult
+            }
+        end
+    end,
+    in_pool = function(self, args) --equivalent to `enhancement_gate = 'm_card'`
+        for _, playing_card in ipairs(G.playing_cards or {}) do
+            if SMODS.has_enhancement(playing_card, 'm_stone') then
+                return true
             end
-            return { x_mult = card.ability.extra.x_mult > 1 and card.ability.extra.x_mult }
         end
-
-        if context.cardarea == G.jokers and context.joker_main then
-            return { x_mult = card.ability.extra.x_mult > 1 and card.ability.extra.x_mult }
-        end
+        return false
     end
 }
 
@@ -63,7 +71,7 @@ SMODS.Joker {
     },
     atlas = 'TienditaJokers',
     rarity = 4, --rareza 1=comun 2=inusual 3=raro 4=legendario
-    cost = 1, --cuanto vale en la tienda
+    cost = 20, --cuanto vale en la tienda
     blueprint_compat = true, 
     eternal_compat = true,
     unlocked = true, --Desbloqueado por default
@@ -82,3 +90,33 @@ SMODS.Joker {
     end,
 }
 
+SMODS.Joker {
+    key = 'j_bust',
+    loc_txt = {
+        name = "Bust Joker", --Nombre
+        text = { --Informacion sobre lo q hace
+            "Retrigger all played {C:attention}Stone{} cards",
+        },
+    },
+    atlas = 'TienditaJokers',
+    rarity = 2, --rareza 1=comun 2=inusual 3=raro 4=legendario
+    cost = 6, --cuanto vale en la tienda
+    blueprint_compat = true, 
+    unlocked = true, --Desbloqueado por default
+    discovered = true, --Descubierto por default
+    pos = { x = 3, y = 0}, --Posicion asset
+    config =  { extra = { repetitions = 1 } },
+     -- Parametros
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {card.ability.extra.repetitions}
+        }
+    end,
+    calculate = function(self, card, context)
+        if context.repetition and context.cardarea == G.play and SMODS.has_enhancement(context.other_card, "m_stone") then
+            return {
+                repetitions = card.ability.extra.repetitions
+            }
+        end
+    end,
+}
